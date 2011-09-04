5add32e5ef8f21393f88096a9e006dbc828802a7
Guillaume Mathieu <contact@guillaumemathieu.com>
2011-09-04 11:23:06 -0400

    added view helper abstract

---
+++ library/Mg/View/Helper/AbstractHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+class Mg_View_Helper_AbstractHelper extends Zend_View_Helper_Abstract
+{
+    private $_options;
+    private $_attributes;
+
+    public function setOptions(array $options)
+    {
+        $this->_options = $options;
+    }
+
+    public function getOption($key, $defaultValue = null)
+    {
+        if (isset($this->_options[$key]) && null !== $this->_options[$key]) {
+            return $this->_options[$key];
+        } else {
+            return $defaultValue;
+        }
+    }
+
+    public function addAttribute($name, $value = '')
+    {
+        $this->_attributes[$name] = $value;
+    }
+
+    public function setAttribute($name, $value)
+    {
+        $this->_attributes = array();
+        $this->addATtribute($name, $value);
+    }
+
+    public function setAttributes(array $attributes)
+    {
+        $this->_attributes = $attributes;
+    }
+
+    public function renderAttributes()
+    {
+        if (is_array($this->_attributes)) {
+
+            $output = array();
+
+            foreach ($this->_attributes as $name => $value) {
+                if (empty($value)) {
+                    $output[] = $name;
+                } else {
+                    $output[] = " {$name}=\"{$value}\"";
+                }
+            }
+
+            return implode(' ', $output);
+        } else {
+            return '';
+        }
+    }
+}
